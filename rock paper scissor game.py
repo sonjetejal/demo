daf2807b852562b38926871d6e8693fd809cc66a
sonjetejal <tejalsonje2002@gmail.com>
2025-11-20 11:46:51 +0530

    Refactor rock paper scissor game and cleanup code

--- rock paper scissor game.py
+++ rock paper scissor game.py
@@ -43,22 +43,13 @@
 </project>
 
 
+  cmd= " cp - r * /var/www/html/new "
 
+" sudo chown -R jenkins: Jenkins var/www/html/new"
 
-touch index.html
-touch dockerfile
-sudo docker build -t folder name .
-sudo docker run -p 8081:80 -d folder name
-
-docker file madhye type kharych
-FROM httpd:2.4
-COPY index.html /usr/local/apache2/htdocs/
-
-index.html madhye html code type kharych
-
-go to browser and type localhost:8081
-        
+   Type= 0.0.0.0/new/index.html/     
 
     
+
 
 
